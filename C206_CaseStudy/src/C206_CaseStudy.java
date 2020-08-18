3ba9a420de6d4333d8c77748ae2f07a6b2335e23
19029087 <19029087@DESKTOP-CM57HC0>
2020-08-18 10:05:44 +0800

    Menu

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -3,7 +3,29 @@ public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int option = 0;
 
+		int exit = 6;
+		while (option != exit) {
+			C206_CaseStudy.menu();
+			
+		}
+	}
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
+	}
+	public static void menu() {
+		C206_CaseStudy.setHeader("RESOURCE CENTRE APP");
+		System.out.println("1. Currency");
+		System.out.println("2. Amount of holding ");
+		System.out.println("3. Loan item");
+		System.out.println("4. Return item");
+		System.out.println("5. Transaction");
+		System.out.println("6. Quit");
+		Helper.line(80, "-");
 	}
 
 }
